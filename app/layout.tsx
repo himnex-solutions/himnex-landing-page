070645ad857085ad649befc1ea7d6749d0992d6c
aakashyadav-coder <aakashyadav94486@gmail.com>
2026-05-10 00:15:25 +0545

    layout updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 import type { Metadata, Viewport } from "next";
 import { Sora } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 import ScrollToTop from "@/components/ScrollToTop";
 import StructuredData from "@/components/StructuredData";
 import { createPageMetadata } from "@/lib/seo";
+
+const GA_ID = "G-9ETJE0JRMC";
 
 const sora = Sora({
   variable: "--font-sora",
@@ -34,6 +37,20 @@ export default function RootLayout({
         <StructuredData />
         {children}
         <ScrollToTop />
+
+        {/* Google Analytics 4 */}
+        <Script
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`}
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${GA_ID}', { page_path: window.location.pathname });
+          `}
+        </Script>
       </body>
     </html>
   );
